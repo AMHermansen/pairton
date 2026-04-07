031042542c927863c895eef7303aa7729cf8bd74
Andreas Hermansen <mail@andreashermansen.dk>
2026-04-07 15:45:14 +0200

    minor fix

---
+++ src/pairton/models/__init__.py
@@ -0,0 +1,6 @@
+from .bias_transformer_wrapper import BiasTransformerWrapper
+from .pairformer_wrapper import PairFormerWrapper
+from .transformer_wrapper import TransformerWrapper
+
+
+__all__ = ["BiasTransformerWrapper", "PairFormerWrapper", "TransformerWrapper"]
